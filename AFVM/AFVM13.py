9a40765a9f0627d83d7333be812467154efde193
Endvad <39588134+Endvad@users.noreply.github.com>
2022-11-23 10:41:46 +0100

    Update AFVM13.py

--- AFVM/AFVM13.py
+++ AFVM/AFVM13.py
@@ -2,7 +2,7 @@ from PIL import Image
 img = Image.open("image_niveaux_gris.png")
 hauteur_image = img.height
 largeur_image = img.width
-valeur_seuil = 40 * 40
+valeur_seuil = 40
 valeur_seuil = valeur_seuil*valeur_seuil+valeur_seuil*valeur_seuil
 img2 = Image.new("RGB",(largeur_image,hauteur_image))
 for y in range(hauteur_image):
