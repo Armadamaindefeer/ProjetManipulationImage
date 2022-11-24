f56f5a0d1cf1e6fa9e375705e9ddfbdd7ea2c98e
Endvad <39588134+Endvad@users.noreply.github.com>
2022-11-24 20:26:31 +0100

    Add files via upload

--- AFVM/AFVM.py
+++ AFVM/AFVM.py
@@ -8,28 +8,48 @@ DEFAULT_SOUVENIR_VACANCES = "lib/picture_backups/souvenir-vacances.bmp"
 DEFAULT_TABLEAU	= "lib/picture_backups/tableau.bmp"
 
 def AFVM_1(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction donnant les valeurs RVB du pixel y:235 x:252 d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	r,v,b=img.getpixel((235,252))
 	info(f"Image : {file_path} Pixel: (235,252) canal rouge : {r} canal vert : {v} canal bleu : {b}")
 
 def AFVM_2(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction donnant les valeurs RVB du pixel y:271 x:142 d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	r,v,b=img.getpixel((271,142))
 	info(f"Image : {file_path} Pixel: (271,142) canal rouge : {r} canal vert : {v} canal bleu : {b}")
 
 def AFVM_3(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction changeant le pixel y:250 x:250 en rouge d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	img.putpixel((250,250), (255,0,0))
 	img.save(f"{OUTPUT_DIR}AFVM3_out.png")
 	img.show()
 
 def AFVM_4(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction changeant le pixel y:100 x:250 en bleu d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	img.putpixel((100,250), (0,0,255))
 	img.save(f"{OUTPUT_DIR}AFVM4_out.png")
 	img.show()
 
 def AFVM_5(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction changeant les pixels du rectangle y:200 x:250 à y:260 x:295 en rouge d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	for y in range(200, 250):
 		for x in range(260, 295):
@@ -38,6 +58,10 @@ def AFVM_5(file_path : str = DEFAULT_FILE_PATH) -> None:
 	img.show()
 
 def AFVM_6(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction transformant une image d'origine en image en niveaux de gris (en utilisant la moyenne des couleurs)
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	hauteur_image = img.height
 	largeur_image = img.width
@@ -50,6 +74,10 @@ def AFVM_6(file_path : str = DEFAULT_FILE_PATH) -> None:
 	img.show()
 
 def AFVM_7(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction transformant une image d'origine en image en niveaux de gris (en utilisant la moyenne des couleurs) puis inversant les couleurs
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	hauteur_image = img.height
 	largeur_image = img.width
@@ -63,24 +91,40 @@ def AFVM_7(file_path : str = DEFAULT_FILE_PATH) -> None:
 	img.show()
 
 def AFVM_8(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction transformant une image d'origine en son symétrique suivant un axe vertical
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	img = img.transpose(Image.FLIP_LEFT_RIGHT)
 	img.save(f"{OUTPUT_DIR}AFVM8_out.png")
 	img.show()
 
 def AFVM_9(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction transformant une image d'origine en son symétrique suivant un axe horizontal
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	img = img.transpose(Image.FLIP_TOP_BOTTOM)
 	img.save(f"{OUTPUT_DIR}AFVM9_out.png")
 	img.show()
 
 def AFVM_10(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction applicant une rotation de 90° dans le sens horaire d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	img = img.transpose(Image.ROTATE_270)
 	img.save(f"{OUTPUT_DIR}AFVM10_out.png")
 	img.show()
 
 def AFVM_11(file_path : str = DEFAULT_FILE_PATH) -> None:
+	"""
+	Fonction applicant une rotation désirée dans le sens horaire d'une image donnée
+	Entrées : Emplacement de l'image
+	"""
 	img = Image.open(file_path)
 	while True:
 		answer = int(input("Quelle rotation doit-être appliqué ?\n[1]: 90°\n[2]: 180°\n[3]: 270°\n[1|2|3]:"))
